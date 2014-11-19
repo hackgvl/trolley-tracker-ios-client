879792654419a39a18cc0b13b96b3c5489b605e0
Ryan Poolos <ryanpoolos@Gmail.com>
2014-11-18 19:47:28 -0500

    Initial Commit

---
+++ TrolleyTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrolleyTracker
+//
+//  Created by Ryan Poolos on 11/18/14.
+//  Copyright (c) 2014 Code For Greenville. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
