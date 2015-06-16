86137a8be4ddaeb388654f9528c8f268ec687407
Austin Younts <austinyounts@gmail.com>
2015-06-16 18:52:13 -0400

    Added Service and Model files.

---
+++ TrolleyTracker/TrolleyLocationService.swift
@@ -0,0 +1,9 @@
+//
+//  TrolleyLocationService.swift
+//  TrolleyTracker
+//
+//  Created by Austin Younts on 6/16/15.
+//  Copyright (c) 2015 Code For Greenville. All rights reserved.
+//
+
+import Foundation
---
+++ TrolleyTracker/TrolleyStopService.swift
@@ -0,0 +1,9 @@
+//
+//  TrolleyStopService.swift
+//  TrolleyTracker
+//
+//  Created by Austin Younts on 6/16/15.
+//  Copyright (c) 2015 Code For Greenville. All rights reserved.
+//
+
+import Foundation
